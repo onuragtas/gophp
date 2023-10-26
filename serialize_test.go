3e77c8bff84a493d5ec51b2eaec75a17629b93c0
onuragtas <agtasonur@gmail.com>
2023-10-27 01:57:58 +0300

    time serialize added

--- serialize_test.go
+++ serialize_test.go
@@ -4,21 +4,23 @@ import (
 	"github.com/onuragtas/gophp/serialize"
 	"log"
 	"testing"
+	"time"
 )
 
 type Location struct {
-	Id   int    `json:"id" php:"id"`
-	Name string `json:"name" php:"name"`
+	Id   int       `json:"id" php:"id"`
+	Name string    `json:"name" php:"name"`
+	T    time.Time `json:"t" php:"t"`
 }
 
 func TestSerialize(t *testing.T) {
 	var locations []Location
-	locations = append(locations, Location{Id: 1, Name: "a"})
-	locations = append(locations, Location{Id: 2, Name: "b"})
+	locations = append(locations, Location{Id: 1, Name: "a", T: time.Now()})
+	locations = append(locations, Location{Id: 2, Name: "b", T: time.Now()})
 
 	test := make(map[string]interface{})
 	test["locations"] = []int{1, 2, 3, 4, 5}
 
-	marshal, err := serialize.Marshal(test)
+	marshal, err := serialize.Marshal(locations)
 	log.Println(string(marshal), err)
 }
